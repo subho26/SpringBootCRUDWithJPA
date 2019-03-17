bddfb294b11c78d44063cbc3edd379fcc7cc69ed
Shubhendu Pramanik <subho.pramanik1@gmail.com>
2019-03-17 09:31:23 +0530

    Used GetMapping

--- src/main/java/com/techchefs/test/controller/StudentController.java
+++ src/main/java/com/techchefs/test/controller/StudentController.java
@@ -5,10 +5,11 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,19 +23,19 @@ public class StudentController {
 	@Qualifier(value="studentService")
 	private StudentService studentService;
 
-	@RequestMapping(value = "/students", method = RequestMethod.POST, consumes = "application/json")
+	@PostMapping(value = "/students", consumes = "application/json")
 	@ResponseStatus(HttpStatus.OK)
 	public Student createNewStudent(@RequestBody @Valid Student student) {
 		return studentService.createStudent(student);
 	}
 
-	@RequestMapping(value = "/student/{id}", method = RequestMethod.GET)
+	@GetMapping(value = "/student/{id}")
 	@ResponseStatus(HttpStatus.OK)
 	public Student getStudentById(@PathVariable Long id) {
 		return studentService.getStudentById(id);
 	}
 
-	@RequestMapping(value = "/student/{id}", method = RequestMethod.DELETE)
+	@DeleteMapping(value = "/student/{id}")
 	@ResponseStatus(HttpStatus.OK)
 	public void deleteStudentById(@PathVariable Long id) {
 		studentService.deleteByRollNumber(id);
